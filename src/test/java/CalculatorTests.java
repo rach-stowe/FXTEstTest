565274c042917180ad85373e81f70d8956a01315
Rachel <Rachel@Rachels-MacBook-Pro.local>
2024-04-29 10:14:49 -0400

    setup for exam 3 prep

---
+++ src/test/java/CalculatorTests.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTests
+{
+	@BeforeEach
+	void setUp() throws Exception
+	{
+
+	}
+
+	@Test
+	void test()
+	{
+
+		
+	}
+}
